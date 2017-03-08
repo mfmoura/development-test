f70d95cf71286af36c9bac32011aee1e67c5b924
Marcelo Francisco <marcelofmoura2@gmail.com>
2017-03-08 00:00:27 -0300

    Implementaão
    Funções implementadas e sem erros: entrega de troco, sem limites

--- php/Troco.php
+++ php/Troco.php
@@ -13,9 +13,9 @@ class Troco
      * @param $reais Valor em reais, podendo conter centavos.
      * @return array Quantidade de notas, indexada pelo seu valor.
      */
-    public function getQtdeNotas($reais)
-    {
-        $qtdeNotas = [
+
+    // Quantidade de notas a retornar
+    private $qtdeNotas = [
           '100' => 0,
            '50' => 0,
            '20' => 0,
@@ -29,14 +29,57 @@ class Troco
          '0.01' => 0,
         ];
 
-        // colocar o código aqui, exemplo:
-        if ($reais == 112.1) {
-            $qtdeNotas['100'] = 1;
-            $qtdeNotas['10'] = 1;
-            $qtdeNotas['2'] = 1;
-            $qtdeNotas['0.1'] = 1;
+    // Valores das notas, em uma array contendo apenas números para não causar erros no PHP fazendo calculos com strings
+    private $notas = array(100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.1, 0.01);
+
+    // Quantidade de notas disponíveis
+    public $notasDisponiveis = [
+           '100' => 0,
+           '50' => 0,
+           '20' => 0,
+           '10' => 0,
+            '5' => 0,
+            '2' => 0,
+            '1' => 0,
+          '0.5' => 0,
+         '0.25' => 0,
+          '0.1' => 0,
+         '0.01' => 0,
+        ];
+
+    // Atributo para pegar o valor que não tem em notas disponível, caso haja
+    private $resto;
+
+    // Calcula a menor quantidade de notas a serem entregues dentro da disponibilidade do objeto
+    public function getQtdeNotas($reais)
+    {
+
+        // Enquanto não tiver dado todo o troco, e ainda ter dinheiro em caixa, com a maior nota disponível podendo pagar o que sobrou
+        while ($reais > 0 && array_sum($this->notasDisponiveis) > $reais){            
+            foreach ($this->notas as $nota) {
+                
+                while ($nota <= $reais && $this->notasDisponiveis[(string)$nota] > 0) {
+
+                    echo $reais = $reais - $nota;
+                    echo PHP_EOL;
+                    $this->qtdeNotas[(string)$nota]++;
+                    $this->notasDisponiveis[(string)$nota]--;
+
+                }
+            }
+
+            if(array_keys($this->notas)){
+                break;
+            }
         }
 
-        return $qtdeNotas;
+        $this->resto = $reais;
+
+        return $this->qtdeNotas;
+    }
+
+    // Retorna o valor que não pode ser pago
+    public function retornaResto(){
+        return $this->resto;
     }
 }
--- php/console.php
+++ php/console.php
@@ -10,6 +10,20 @@ $reais = $argv[1] ?: 0;
 
 $troco = new Troco();
 
+$troco->notasDisponiveis['100'] = 0;
+$troco->notasDisponiveis['50'] = 0;
+$troco->notasDisponiveis['25'] = 0;
+$troco->notasDisponiveis['10'] = 0;
+$troco->notasDisponiveis['5'] = 0;
+$troco->notasDisponiveis['2'] = 0;
+$troco->notasDisponiveis['1'] = 2;
+$troco->notasDisponiveis['0.5'] = 5;
+$troco->notasDisponiveis['0.25'] = 25;
+$troco->notasDisponiveis['0.1'] = 50;
+$troco->notasDisponiveis['0,01'] = 100;
+
 $notas = $troco->getQtdeNotas($reais);
+$resto = $troco->retornaResto();
 
 print_r($notas);
+print_r($resto);
