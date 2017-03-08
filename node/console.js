1e27e0c3c440415d7046161daceb10813b08ab8f
Marcelo Francisco <marcelofmoura2@gmail.com>
2017-03-07 21:48:49 -0300

    Apagando
    Apagando arquivos que não irei utilizar

--- node/console.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-'use strict';
-const Troco = require('./Troco.js')
-let troco = new Troco()
-let notas = troco.getQtdeNotas(process.argv[2] || 0)
-console.log(notas)
--- ruby/console.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-
-require(File.join('.', File.dirname(__FILE__), 'troco.rb'))
-notas = Troco.new.get_qtd_notas (ARGV[0] || 0).to_f
-p notas
